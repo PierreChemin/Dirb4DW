9a9cc9bd00fe0044e2c07bf5b98c8db07655cabd
PierreChemin <pierre.chemin@efrei.net>
2024-02-16 15:58:29 +0100

    retouche sur la boucle for

--- dirD.py
+++ dirD.py
@@ -24,9 +24,27 @@ def main():
     clear_file(report)
 
     with open(url2test,"r", encoding='utf-8') as f1:
+        for url in f1:
+            url = url.strip()
+            with open(wordlist, "r", encoding='utf-8') as f2:
+                for page in f2:
+                    page = page.strip()  
+                    fullURL = purgeBack(url + '/' + page)
+                    print(fullURL, "before request")
+                    #request = session.get(fullURL)
+                    subprocess.run(['curl', '--socks5-hostname', 'localhost:9150', fullURL])
+                    print("after request")
+                    if '200' in str(request) or '301' in str(request):
+                        fullURL += "\n"
+                        print("ok")
+
+    return 0
+
+"""    with open(url2test,"r", encoding='utf-8') as f1:
         with open(wordlist, "r", encoding='utf-8') as f2:
             for url in f1:
-                url = url.strip()  
+                url = url.strip()
+                print(url)
                 for page in f2:
                     page = page.strip()  
                     fullURL = purgeBack(url + '/' + page)
@@ -35,9 +53,9 @@ def main():
                     if '200' in str(request) or '301' in str(request):
                         fullURL += "\n"
                         with open(report, "a+", encoding='utf-8') as f3:
-                            f3.write(fullURL)  
+                            f3.write(fullURL)  """
+                    
 
-    return 0
 
 if __name__ == "__main__":
     main()
